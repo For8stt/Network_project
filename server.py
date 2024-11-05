4f0949a9a89d82f071d141ba4c4cde198edf64e9
For8stt <xkisil@stuba.sk>
2024-11-05 15:21:52 +0100

    added crc and keep alive function

--- server.py
+++ server.py
@@ -9,30 +9,64 @@ import time
 import os
 
 MAX_UDP_SIZE = 1472
-HEADER_LENGTH=12
+HEADER_LENGTH=14
 SAVE_DIRECTORY='/Users/ulian/PycharmProjects/pksPraktika/receive'
 
 
 CLIENT_MY_IP = "127.0.0.1"
-CLIENT_MY_PORT = 50601
+CLIENT_MY_PORT = 50602
 
 CLIENT_SENT_IP = "127.0.0.1"
-CLIENT_SENT_PORT = 50602
+CLIENT_SENT_PORT = 50601
 
-class Server:
+# CLIENT_MY_IP = None
+# CLIENT_MY_PORT = None
+# CLIENT_SENT_IP = None
+# CLIENT_SENT_PORT = None
+
+class Client2:
     WINDOW_SIZE = 4
 
     def __init__(self, ip, port, receiver_ip, receiver_port):
-        self.receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket for receiving
-        self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket for sending
+        self.receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.receiver_ip = receiver_ip
         self.receiver_port = receiver_port
-        self.receive_sock.bind((ip, port))  # Bind to IP and port
+        self.receive_sock.bind((ip, port))
         self.running = True
         self.connected = False
 
+        self.receive_thread = threading.Thread(target=self.receive)
+        self.receive_thread.daemon = True
+        self.receive_thread.start()
+
         self.window = {}
         self.base = 0
+
+        self.keep_alive_interval = 5
+        self.heartbeat_timeout = 3
+        self.heartbeat_sent_count = 0
+        self.last_heartbeat_time = time.time()
+        self.heartbeat_thread = threading.Thread(target=self.send_heartbeats)
+        self.heartbeat_thread.daemon = True
+        self.heartbeat_thread.start()
+
+    def send_heartbeats(self):
+        while self.running:
+            time.sleep(self.keep_alive_interval)
+            if not self.connected:
+                continue
+
+            current_time = time.time()
+            if current_time - self.last_heartbeat_time >= self.keep_alive_interval:
+                heartbeat_header = self.make_header(0, 1, 5, 0)  # 5  heartbeat
+                self.send_sock.sendto(heartbeat_header, (self.receiver_ip, self.receiver_port))
+                self.heartbeat_sent_count += 1
+
+                if self.heartbeat_sent_count >= self.heartbeat_timeout:
+                    print("The connection is lost. No response to heartbeat.")
+                    self.running = False
+                    self.close()
 
     def receive(self):
         while self.running:
@@ -41,66 +75,90 @@ class Server:
 
             expected_seq_num = 0
             window = {}
-
+            ###=======================================for receive=======================================####
             while True:
                 try:
                     data, addr = self.receive_sock.recvfrom(MAX_UDP_SIZE)
-                    header = struct.unpack('!III', data[:HEADER_LENGTH])  # Розпакування заголовка
+                    header = struct.unpack('!IIIH', data[:HEADER_LENGTH])
 
-                    fragment_number, total_fragments, message_type = header
+                    fragment_number, total_fragments, message_type, crc_received = header
+                    crc_calculated = self.crc16(data[HEADER_LENGTH:])
 
-                    if message_type == 4:  # Якщо отримано ACK
+                    if crc_calculated != crc_received and message_type in {0 , 1}:
+                        print(
+                            f"Received fragment {fragment_number + 1} with CRC error. Expected: {crc_received}, Calculated: {crc_calculated}")
+                        continue
+                    elif message_type in {0 , 1}:
+                        # print(f"Received fragment {fragment_number + 1} without CRC error. Expected: {crc_received}, Calculated: {crc_calculated}")
+                        pass
+
+                    if message_type == 4:
                         print(f"Received ACK for fragment {fragment_number + 1}")
-                        del self.window[fragment_number]  # Видаляємо фрагмент з вікна
+                        del self.window[fragment_number]
                         if fragment_number == self.base:
-                            self.base += 1  # Зсуваємо вікно
+                            self.base += 1
 
-                    fragment_data = data[HEADER_LENGTH:]  # Відокремлюємо дані фрагмента
+                    if message_type == 6:  # 6 heartbeat reply
+                        # print("Received a response to heartbeat.")
+                        self.last_heartbeat_time = time.time()
+                        self.heartbeat_sent_count = 0
+                        continue
+                    if message_type == 5:  # 5  heartbeat
+                        # print("Received heartbeat.")
+                        self.last_heartbeat_time = time.time()
+                        self.heartbeat_sent_count = 0
+
+                        heartbeat_header = self.make_header(0, 1, 6, 0)  # 5  heartbeat
+                        self.send_sock.sendto(heartbeat_header, (self.receiver_ip, self.receiver_port))
+
+                        continue
+
+                    fragment_data = data[HEADER_LENGTH:]
                     if len(received_fragments) == 0:
                         start_time = time.time()
-                    if message_type == 2:
+                    if message_type == 2:  # handshake
                         break
 
-                    if message_type == 3:
+                    if message_type == 3:  # NAME FILE
                         file_name = fragment_data.decode('utf-8')
                         continue
 
-                    if message_type == 1:  # Прийом файлу
-                        # Якщо фрагмент у правильному порядку
+                    if message_type == 1:  # FILE
                         if fragment_number == expected_seq_num:
                             received_fragments[fragment_number] = fragment_data
                             expected_seq_num += 1
 
-                            # Перевірка буферу для непослідовних фрагментів
                             while expected_seq_num in window:
                                 received_fragments[expected_seq_num] = window.pop(expected_seq_num)
                                 expected_seq_num += 1
                         elif fragment_number > expected_seq_num:
                             window[fragment_number] = fragment_data
 
-                        # Відправляємо ACK для фрагмента
-                        ack_header = self.make_header(fragment_number, total_fragments, 4)  # 4 для ACK
+                        ack_header = self.make_header(fragment_number, total_fragments, 4, 0)  # 4  ACK
                         self.send_sock.sendto(ack_header, (self.receiver_ip, self.receiver_port))
                         print(f"Sent ACK for fragment {fragment_number + 1}/{total_fragments}")
-                    if message_type == 0:
+                    if message_type == 0:  # TEXT
                         received_fragments[fragment_number] = fragment_data
 
-                    # Якщо всі фрагменти отримані
                     if len(received_fragments) == total_fragments:
                         break
                 except socket.timeout:
-                    # Повторно відправляємо фрагменти з таймаутом
+
                     for seq_num in self.window:
 
                         packet, send_time = self.window[seq_num]
-                        if time.time() - send_time > 3:  # Таймаут для повторної відправки
+                        if time.time() - send_time > 3:
                             print(f"Timeout for fragment {seq_num + 1}, resending...")
                             self.send_sock.sendto(packet, (CLIENT_SENT_IP, CLIENT_SENT_PORT))
-
-            if not message_type == 2:
+                except Exception as e:
+                    self.running = False
+                    self.connected = False
+                    break
+                ###==================================================================================####
+            if message_type in {0, 1, 3}:
                 complete_message = b''.join(received_fragments[i] for i in range(total_fragments))
 
-            elapsed_time = time.time() - start_time  # Вимірюємо час передачі
+            elapsed_time = time.time() - start_time
             if message_type == 0:
                 print(f"Received file of size: {len(complete_message)} bytes in {elapsed_time:.5f} seconds.")
                 print(f"Received message: {complete_message.decode('utf-8', errors='ignore')}")
@@ -134,37 +192,48 @@ class Server:
             self.send_handshake()
 
         while self.running:
-            message = input("Input message to server (or '1' to send a file):  ")
+            try:
+                message = input("Input message to server (or '1' to send a file):  ")
+                if self.running == False:
+                    break
+                if not self.running and not self.connected:
+                    break
 
-            if not self.running and not self.connected:
-                break
+                if not self.connected:
+                    self.send_handshake()
+                    print("Failed to connect to a user ..")
+                    continue
 
-            if not self.connected:
-                self.send_handshake()
-                print("Failed to connect to a user ..")
-                continue
+                if message == '1':
+                    self.choose_file_and_send()
+                    continue
+                if message == 'quit':
+                    self.connected = False
+                    self.running = False
 
-            if message == '1':
-                self.choose_file_and_send()
-                continue
-            if message == 'quit':
-                self.connected = False
+                message_bytes = message.encode('utf-8')
+                total_length = len(message_bytes)
 
-            message_bytes = message.encode('utf-8')
-            total_length = len(message_bytes)
+                num_fragments = (total_length // (MAX_UDP_SIZE - HEADER_LENGTH)) + 1
+                print(f"Total size: {total_length} bytes")
+                print(f"Number of fragments: {num_fragments}")
+                for i in range(num_fragments):
+                    start = i * (MAX_UDP_SIZE - HEADER_LENGTH)
+                    end = min(start + (MAX_UDP_SIZE - HEADER_LENGTH), total_length)
+                    fragment = message_bytes[start:end]
 
-            num_fragments = (total_length // (MAX_UDP_SIZE - HEADER_LENGTH)) + 1
-            print(f"Total size: {total_length} bytes")
-            print(f"Number of fragments: {num_fragments}")
-            for i in range(num_fragments):
-                start = i * (MAX_UDP_SIZE - HEADER_LENGTH)
-                end = min(start + (MAX_UDP_SIZE - HEADER_LENGTH), total_length)
-                fragment = message_bytes[start:end]
+                    crc = self.crc16(fragment)
 
-                header = self.make_header(i, num_fragments, 0)
-                packet = header + fragment
-                self.send_sock.sendto(packet, (CLIENT_SENT_IP, CLIENT_SENT_PORT))
-                print(f"Sent fragment {i + 1}/{num_fragments}: {len(fragment)} bytes")
+                    header = self.make_header(i, num_fragments, 0, crc)
+                    packet = header + fragment
+                    self.send_sock.sendto(packet, (CLIENT_SENT_IP, CLIENT_SENT_PORT))
+                    print(f"Sent fragment {i + 1}/{num_fragments}: {len(fragment)} bytes")
+            except OSError as e:
+                print(f"Error sending packet: {e}")
+                self.running = False
+            except Exception as e:
+                print(f"Error during receiving: {e}")
+                self.running = False
 
     def choose_file_and_send(self):
         root = Tk()
@@ -183,57 +252,72 @@ class Server:
             print(f"File Name: {name_file.decode('utf-8')}")
             print(f"Total Size: {total_length} bytes")
 
-            name_header = self.make_header(0, 1, 3)
+            name_header = self.make_header(0, 1, 3, 0)
             name_packet = name_header + name_file
             self.send_sock.sendto(name_packet, (CLIENT_SENT_IP, CLIENT_SENT_PORT))
 
             num_fragments = (total_length // (MAX_UDP_SIZE - HEADER_LENGTH)) + 1
 
-
-            next_seq_num = 0  # Наступний порядковий номер для відправлення
+            next_seq_num = 0
 
             while self.base < num_fragments:
-                # Відправляємо фрагменти, якщо є місце у вікні
                 while next_seq_num < self.base + self.WINDOW_SIZE and next_seq_num < num_fragments:
                     start = next_seq_num * (MAX_UDP_SIZE - HEADER_LENGTH)
                     end = min(start + (MAX_UDP_SIZE - HEADER_LENGTH), total_length)
                     fragment = file_data[start:end]
 
-                    # Створюємо заголовок з порядковим номером
-                    header = self.make_header(next_seq_num, num_fragments, 1)
+                    crc = self.crc16(fragment)
+                    print(crc)
+                    print(fragment)
+                    header = self.make_header(next_seq_num, num_fragments, 1, crc)
                     packet = header + fragment
                     self.send_sock.sendto(packet, (CLIENT_SENT_IP, CLIENT_SENT_PORT))
 
-
                     print(f"Sent fragment {next_seq_num + 1}/{num_fragments}: {len(fragment)} bytes")
-                    self.window[next_seq_num] = (packet, time.time())  # Зберігаємо фрагмент і час відправлення
+                    self.window[next_seq_num] = (packet, time.time())
                     next_seq_num += 1
 
-                # Чекаємо на ACK або таймаут
                 self.receive_sock.settimeout(3)
 
+    def crc16(self, data: bytes) -> int:
+        crc = 0xFFFF
+        for byte in data:
+            crc ^= byte
+            for _ in range(8):
+                if crc & 0x0001:
+                    crc >>= 1
+                    crc ^= 0xA001
+                else:
+                    crc >>= 1
+        return crc
+
     def send_handshake(self):
-        packet = self.make_header(0, 1, 2)
+        packet = self.make_header(0, 1, 2, 0)
         self.send_sock.sendto(packet, (CLIENT_SENT_IP, CLIENT_SENT_PORT))
 
-    def make_header(self, fragment_number, num_fragments, message_type):  # MESSAGE_TYPE_TEXT=0 text,  1 is file
-        return struct.pack('!III', fragment_number, num_fragments, message_type)
+    ###=======I — unsigned int, 4 ₴₴ H=2bite ₴₴ B=1bite ₴₴ Q=8bite ==========###
+    def make_header(self, fragment_number, num_fragments, message_type, crc):
+        return struct.pack('!IIIH', fragment_number, num_fragments, message_type, crc)
 
     def close(self):
+        print("Closing connection...")
+        self.running = False
         self.receive_sock.close()
-        print("Client closed..")
+        self.send_sock.close()
+        # self.receive_thread.join()
+        print("Connection closed.")
 
 
 if __name__ == "__main__":
-    client = Server(CLIENT_MY_IP, CLIENT_MY_PORT, CLIENT_SENT_IP, CLIENT_SENT_PORT)
+    # print("!!!do not forget to change the path where to save the received files in the parameters!!!");
+    #
+    # CLIENT_MY_PORT = int(input("Enter your source port: "))
+    # CLIENT_SENT_PORT = int(input("Enter destination port: "))
+    # CLIENT_SENT_IP = input("Enter destination IP: ")
+    # CLIENT_MY_IP = input("Enter your IP: ")
 
-    # Start threads for receiving
-    receive_thread = threading.Thread(target=client.receive)
-    receive_thread.daemon = True  # робимо потік демонічним
-    receive_thread.start()
+    client = Client2(CLIENT_MY_IP, CLIENT_MY_PORT, CLIENT_SENT_IP, CLIENT_SENT_PORT)
 
-    # Main thread handles sending
     client.send()
 
-    receive_thread.join()
     client.close()
